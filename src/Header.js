5c1ab7619a538ef353a710e7159421ddb40a82d6
Harri Kurniawan <kurniawanharry165@gmail.com>
2021-09-30 15:13:49 +0700

    clean code & minor fixed
    pertemuan 1 2

---
+++ src/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <div>
+            <h2>Header ini menggunakan rfac</h2>
+        </div>
+    )
+}
---
+++ src/Variabel.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Variabel extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Variabel ini menggunakan rcc</h2>
+            </div>
+        )
+    }
+}
